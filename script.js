6d8ee00b0f82e7b5635b2e9078760dd0c62c5776
Karol Michalik <bel1nqone@gmail.com>
2024-01-27 17:24:37 +0100

    update
    adding JS for hamburger menu and adding first sticky navbar

--- script.js
+++ script.js
@@ -2,12 +2,32 @@
 const hamIcon = document.querySelector(".hamburger-icon");
 const hamMenuLinks = document.querySelector(".menu-ham-links");
 
+/* function for display menu links */
 hamIcon.addEventListener("click", function () {
-  if (hamIcon === "click") {
+  if (hamMenuLinks.style.display === "block") {
+    hamMenuLinks.style.display = "none";
+  } else {
     hamMenuLinks.style.display = "block";
   }
 });
+
 /* STICKY navigation */
+const desktopNavbar = document.querySelector("#desktop-nav");
+const hamburgerNavbar = document.querySelector("#hamburger-nav");
+const main = document.querySelector("main");
+
+const mainPos = main.getBoundingClientRect().top;
+
+window.addEventListener("scroll", function () {
+  let scrollPos = window.scrollY;
+  if (scrollPos >= mainPos) {
+    desktopNavbar.classList.add("sticky");
+    hamburgerNavbar.classList.add("sticky");
+  } else {
+    desktopNavbar.classList.remove("sticky");
+    hamburgerNavbar.classList.remove("sticky");
+  }
+});
 
 /* order pop out */
 const orderPop = document.querySelector(".order-pop-out");
